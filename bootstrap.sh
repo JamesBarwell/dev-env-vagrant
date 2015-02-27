3a1744dc504368335b040ffa7c08c06d793f5c7b
James Barwell <jb@jamesbarwell.co.uk>
2015-02-27 10:25:07 +0000

    Update to latest packages and config

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,13 @@
 apt-get update
 
 # Install useful packages
-apt-get install -y vim git curl
+apt-get install -y vim git curl build-essential virtualbox-guest-utils lsof tmux strace python-pip
 
 # Install nodejs
 apt-get install -y software-properties-common python-software-properties
-add-apt-repository ppa:chris-lea/node.js 
+add-apt-repository ppa:chris-lea/node.js
 apt-get update
 apt-get install -y nodejs
+
+# Install Ansible
+pip install ansible
--- settings.sh
+++ settings.sh
@@ -5,6 +5,11 @@ fi
 cp /tmp/dotfiles/.vimrc ~/
 cp /tmp/dotfiles/.gitconfig ~/
 
+# Set up bash
+if ! grep -q "completion-ignore-case" ~/.bashrc; then
+    echo 'bind "set completion-ignore-case on"' >> ~/.bashrc
+fi
+
 # Set up vim
 if [ ! -d "~/.vim/bundle/Vundle.vim" ]; then
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
