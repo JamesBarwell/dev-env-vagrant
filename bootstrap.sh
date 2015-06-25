37877b1c8350427f178e3a923c2e05cb3d308919
James Barwell <jb@jamesbarwell.co.uk>
2015-06-25 14:51:46 +0000

    Install node 0.12 and docker

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,14 +3,14 @@
 # Update packages
 apt-get update
 
-# Install useful packages
+# Install from apt
 apt-get install -y vim git curl build-essential virtualbox-guest-utils lsof tmux strace python-pip
 
-# Install nodejs
-apt-get install -y software-properties-common python-software-properties
-add-apt-repository ppa:chris-lea/node.js
-apt-get update
+# Install from web
+cp /vagrant/install-scripts/* /tmp/
+/tmp/install-node-0.12
 apt-get install -y nodejs
+/tmp/install-docker
 
-# Install Ansible
+# Install from pip
 pip install ansible
