5d3792be7257bfdbfe764942f6de9beea190e3c6
James Barwell <jb@jamesbarwell.co.uk>
2015-11-24 10:59:38 +0000

    Add openssl and user-scripts copy

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,14 +4,19 @@
 apt-get update
 
 # Install from apt
-apt-get install -y vim git curl build-essential virtualbox-guest-utils lsof tmux strace python-pip
+apt-get install -y vim git curl build-essential virtualbox-guest-utils lsof tmux strace python-pip openssl
 
 # Install from web
 cp /vagrant/install-scripts/* /tmp/
+#node
 /tmp/install-node-0.12
 apt-get install -y nodejs
+#docker
 /tmp/install-docker
 usermod -aG docker vagrant
 
 # Install from pip
 pip install ansible
+
+# Set up useful scripts
+cp /vagrant/user-scripts/* /usr/local/bin
